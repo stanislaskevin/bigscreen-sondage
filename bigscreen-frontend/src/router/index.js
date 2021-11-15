bb869068518623cf540503f6ae0e6b4954833b81
stan_codes <stan_codes@MacBook-Pro-de-stan-codes.local>
2021-11-15 03:37:22 +0100

    Building the Vue front-end

--- bigscreen-frontend/src/router/index.js
+++ bigscreen-frontend/src/router/index.js
@@ -1,19 +1,39 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/Home.vue';
+import Sondage from '../views/Sondage.vue';
+import Reponse from '../views/Reponse.vue';
+
+import Home from '../views/admin/Home.vue';
+import Questionnaires from '../views/admin/Questionnaires.vue';
+import Reponses from '../views/admin/Reponses.vue';
 
 const routes = [
+  // Route Client
   {
     path: '/',
+    name: 'Sondage',
+    component: Sondage,
+  },
+  {
+    path: '/reponse/:id',
+    name: 'Reponse',
+    component: Reponse,
+  },
+
+  // Route Admin
+  {
+    path: '/admin',
     name: 'Home',
     component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '/admin/questionnaires',
+    name: 'Questionnaires',
+    component: Questionnaires,
+  },
+  {
+    path: '/admin/reponses',
+    name: 'Reponses',
+    component: Reponses,
   },
 ];
 
@@ -21,5 +41,17 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
 });
+
+// Systeme de private route a mettre en place
+/** 
+router.beforeEach((to, from, next) => {
+  if (to.name !== 'Login' && !isAuthenticated) next({ name: 'Login' })
+  else next()
+})
+router.afterEach((to, from) => {
+  // ...
+})
+**/
+
 
 export default router;
